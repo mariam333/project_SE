846a8717ebe4cf7b4462c6417a3718d486a991e9
Joumana <joumana.1444@gmail.com>
2020-01-26 12:21:41 +0200

    ll

--- src/main/java/application/PaymentReportController.java
+++ src/main/java/application/PaymentReportController.java
@@ -1,5 +1,88 @@
+/**
+ * Sample Skeleton for 'PaymentReport.fxml' Controller Class
+ */
+
 package src.main.java.application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javax.swing.JOptionPane;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 
 public class PaymentReportController {
 
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="diag_pay"
+    private BarChart<?, ?> diag_pay; // Value injected by FXMLLoader
+      
+    
+    @FXML // fx:id="dateofcomp"
+    private Label dateofcomp; // Value injected by FXMLLoader
+    
+    @FXML
+    void BacktoAllComp(ActionEvent event) throws IOException{
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("StoreManager.fxml"));
+		AnchorPane root = (AnchorPane) loader.load();
+		StoreManager user = loader.getController();
+//		Image im = new Image("images/background.jpg");to me all comp
+//		user.setimage(im);
+//		user.set(MyEmail);
+		Scene regist = new Scene(root);
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(regist);
+		app_stage.show();
+	}
+    
+     void comlaintdata(String Month1,String store1) {
+    	 dateofcomp.setText(Month1);
+    	 String message = "payment#" + "complaint#" + store1 + "#" + month1;
+ 		Connect.client.handleMessageFromClientUI(message);
+ 		String[] Msg = Connect.client.servermsg.split("#");
+ 		if ((Msg[0] == null) || (Msg[1] == null)|| (Msg[2] == null)|| (Msg[3] == null)) {
+			JOptionPane.showMessageDialog(null, "Showing  Failed ! ");
+		} else {
+			 final CategoryAxis xAxis = new CategoryAxis();
+		        final NumberAxis yAxis = new NumberAxis();
+			diag_pay= new BarChart<String,String>(xAxis,yAxis);
+			xAxis.setLabel("Week");       
+	        yAxis.setLabel("Value");
+	        XYChart.Series series1 = new XYChart.Series();
+	        series1.getData().add(new XYChart.Data("first week", Msg[0]));
+	        series1.getData().add(new XYChart.Data("second week", Msg[1]));
+	        series1.getData().add(new XYChart.Data("Third week",Msg[2]));
+	        series1.getData().add(new XYChart.Data("Fourth week", Msg[3]));
+	       // Scene scene  = new Scene(bc,800,600);
+	        diag_pay.getData().addAll(series1);
+	       // stage.setScene(scene);
+	       // stage.show();
+		}
+    	
+     }
+     
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert diag_pay != null : "fx:id=\"diag_pay\" was not injected: check your FXML file 'PaymentReport.fxml'.";
+
+    }
 }
--- src/main/java/application/StoreManagerController.java
+++ src/main/java/application/StoreManagerController.java
@@ -1,5 +1,81 @@
+/**
+ * Sample Skeleton for 'StoreManager.fxml' Controller Class
+ */
+
 package src.main.java.application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 
 public class StoreManagerController {
 
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="Reports"
+    private TableView<?> Reports; // Value injected by FXMLLoader
+
+    @FXML // fx:id="OrderReport"
+    private TableColumn<?, ?> OrderReport; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ComplaintReport"
+    private TableColumn<?, ?> ComplaintReport; // Value injected by FXMLLoader
+
+    @FXML // fx:id="PaymentReport"
+    private TableColumn<?, ?> PaymentReport; // Value injected by FXMLLoader
+
+    @FXML // fx:id="Pranch"
+    private Label Pranch; // Value injected by FXMLLoader
+
+   
+    @FXML
+	void Backtologin(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+		AnchorPane root = (AnchorPane) loader.load();
+		Login user = loader.getController();
+//		Image im = new Image("images/background.jpg");info to hala
+//		user.setimage(im);
+//		user.set(MyEmail);
+		Scene regist = new Scene(root);
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(regist);
+		app_stage.show();
+	}
+    
+    void FullTable(String Store)
+    {
+    	Pranch.setText(Store);
+    	
+    }
+    
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert Reports != null : "fx:id=\"Reports\" was not injected: check your FXML file 'StoreManager.fxml'.";
+        assert OrderReport != null : "fx:id=\"OrderReport\" was not injected: check your FXML file 'StoreManager.fxml'.";
+        assert ComplaintReport != null : "fx:id=\"ComplaintReport\" was not injected: check your FXML file 'StoreManager.fxml'.";
+        assert PaymentReport != null : "fx:id=\"PaymentReport\" was not injected: check your FXML file 'StoreManager.fxml'.";
+        assert Pranch != null : "fx:id=\"Pranch\" was not injected: check your FXML file 'StoreManager.fxml'.";
+
+    }
 }
