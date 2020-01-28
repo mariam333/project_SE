d225f3b8a4c0c1f1faacb354b0d871f2af44a873
joumana <joumana.1444@gmail.com>
2020-01-29 00:50:59 +0200

    message

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,28 @@
+
+package src.main.java.application;
+
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Main extends Application {
+@Override
+public void start(Stage primaryStage) throws IOException {
+// constructing our scene
+URL url = getClass().getResource("Connect.fxml");
+AnchorPane pane = FXMLLoader.load( url );
+Scene scene = new Scene( pane );
+// setting the stage
+primaryStage.setScene( scene );
+primaryStage.show();
+
+}
+     public static void main(String[] args) {
+         launch(args);
+             }
+}
